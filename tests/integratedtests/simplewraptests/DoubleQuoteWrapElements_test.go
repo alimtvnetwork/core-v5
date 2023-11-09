5e6e9f0d777c97e25065da955f6699f66ed08580
Md. Alim Ul Karim <devorg.bd@gmail.com>
2023-11-09 11:02:15 +0600

    Updates

--- tests/integratedtests/simplewraptests/DoubleQuoteWrapElements_test.go
+++ tests/integratedtests/simplewraptests/DoubleQuoteWrapElements_test.go
@@ -8,20 +8,68 @@ import (
 	"gitlab.com/auk-go/core/simplewrap"
 )
 
-func Test_When_DoubleQuoteWrapElements_Called_with(t *testing.T) {
+func Test_When_DoubleQuoteWrapElements_SkipQuoteOnPresent_Should_Only_Have_SingleDoubleQuotation_NotDuplicates(t *testing.T) {
 	testCases := []string{
 		"some-elem",
 		"alim-elem",
 		"\"has-quote\"",
+		"",
+		"\"",
+		"\"first",
+		"last\"",
+		"'",
 		"simple",
 	}
-	expectation := []string{"\"some-elem\"", "\"alim-elem\"", "", "\"simple\""}
+	expectation := []string{
+		"\"some-elem\"",
+		"\"alim-elem\"",
+		"\"has-quote\"",
+		"\"\"",
+		"\"\"",
+		"\"first\"",
+		"\"last\"",
+		"\"'\"",
+		"\"simple\"",
+	}
 	actual := simplewrap.
 		DoubleQuoteWrapElements(true,
 			testCases...)
 	
 	// Assert
-	convey.Convey("Ranges 1-15, RangesExcept(3, 4, 5), should not contain 3,4,5", t, func() {
+	convey.Convey("Wrap strings with double quote, if exists already then skip adding", t, func() {
+		convey.So(actual, should.Equal, expectation)
+	})
+}
+
+func Test_When_DoubleQuoteWrapElements_SkipQuoteOnPresent_Disabled_Should_Have_DuplicateDoubleQuotations(t *testing.T) {
+	testCases := []string{
+		"some-elem",
+		"alim-elem",
+		"\"has-quote\"",
+		"",
+		"\"",
+		"\"first",
+		"last\"",
+		"'",
+		"simple",
+	}
+	expectation := []string{
+		"\"some-elem\"",
+		"\"alim-elem\"",
+		"\"\\\"has-quote\\\"\"",
+		"\"\"",
+		"\"\\\"\"",
+		"\"\\\"first\"",
+		"\"last\\\"\"",
+		"\"'\"",
+		"\"simple\"",
+	}
+	actual := simplewrap.
+		DoubleQuoteWrapElements(false,
+			testCases...)
+	
+	// Assert
+	convey.Convey("Wrap strings with double quote, if exists already then skip adding", t, func() {
 		convey.So(actual, should.Equal, expectation)
 	})
 }
