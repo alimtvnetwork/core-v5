4156dcf06a58b11bb8e05396f109ec4dfc5003ce
Md. Alim Ul Karim <devorg.bd@gmail.com>
2023-11-11 12:50:01 +0600

    Optimized unit tests
    Optimized unit tests

--- tests/integratedtests/simplewraptests/CurlyWrapOptions_test.go
+++ tests/integratedtests/simplewraptests/CurlyWrapOptions_test.go
@@ -3,24 +3,18 @@ package simplewraptests
 import (
 	"testing"
 
-	"github.com/smarty/assertions/should"
-	"github.com/smartystreets/goconvey/convey"
 	"gitlab.com/auk-go/core/coredata/corestr"
-	"gitlab.com/auk-go/core/corevalidator"
-	"gitlab.com/auk-go/core/errcore"
+	"gitlab.com/auk-go/core/internal/testsinternal"
 	"gitlab.com/auk-go/core/simplewrap"
 )
 
 func Test_CurlyWrapOptions_Wraps_All_CheckConditionally_NoDuplicateCurly(t *testing.T) {
-	// Arrange
-	sliceValidator := corevalidator.SliceValidator{
-		ValidatorCoreCondition: corevalidator.DefaultTrimCoreCondition,
-	}
-
 	for caseIndex, testCase := range curlyWrapOptionsValidTestCases {
+		// Arrange
 		inputs := testCase.Arrange()
 		actualSlice := corestr.New.SimpleSlice.Cap(len(inputs))
 
+		// Act
 		for _, input := range inputs {
 			actualSlice.Add(
 				simplewrap.CurlyWrapOption(
@@ -28,37 +22,13 @@ func Test_CurlyWrapOptions_Wraps_All_CheckConditionally_NoDuplicateCurly(t *test
 		}
 
 		finalActual := actualSlice.Strings()
-		testCase.SetActual(finalActual)
-		sliceValidator.SetActual(finalActual)
-		sliceValidator.ExpectedLines = testCase.ExpectedInput.([]string)
-
-		nextBaseParam := corevalidator.ValidatorParamsBase{
-			CaseIndex:          caseIndex,
-			Header:             testCase.Title,
-			IsAttachUserInputs: true,
-			IsCaseSensitive:    true,
-		}
-
-		// Act
-		validationFinalError := sliceValidator.AllVerifyError(
-			&nextBaseParam)
+		finalTestCase := testsinternal.
+			TestCase(testCase.BaseTestCase)
 
 		// Assert
-		convey.Convey(testCase.Title, t, func() {
-			errcore.PrintErrorWithTestIndex(
-				caseIndex,
-				testCase.Title,
-				validationFinalError)
-
-			convey.So(
-				validationFinalError,
-				should.BeNil)
-		})
-
-		convey.Convey(testCase.Title+" - type verify", t, func() {
-			convey.So(
-				testCase.TypeValidationError(),
-				should.BeNil)
-		})
+		finalTestCase.AssertEqual(
+			t,
+			caseIndex,
+			finalActual...)
 	}
 }
