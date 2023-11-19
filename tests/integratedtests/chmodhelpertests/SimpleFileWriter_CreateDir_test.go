8299b5c79e413cb2542efdf41fbaadba7f45509f
Md. Alim Ul Karim <devorg.bd@gmail.com>
2023-11-19 13:01:06 +0600

    Updates

---
+++ tests/integratedtests/chmodhelpertests/SimpleFileWriter_CreateDir_test.go
@@ -0,0 +1,62 @@
+package chmodhelpertests
+
+import (
+	"testing"
+
+	"gitlab.com/auk-go/core/chmodhelper"
+	"gitlab.com/auk-go/core/coredata/corestr"
+	"gitlab.com/auk-go/core/coretests/args"
+	"gitlab.com/auk-go/core/errcore"
+)
+
+func Test_SimpleFileWriter_CreateDir_Verification(t *testing.T) {
+	for caseIndex, testCase := range dirFilesWithContentCreateReadTestCases {
+		// Arrange
+		inputs := testCase.
+			ArrangeInput.([]args.One)
+		actualSlice := corestr.
+			New.
+			SimpleSlice.
+			Cap(len(inputs))
+
+		// Act
+		for i, parameter := range inputs {
+			files := parameter.First.([]chmodhelper.DirFilesWithContent)
+
+			for _, dirFiles := range files {
+				err := dirFiles.Create(true)
+
+				errcore.HandleErr(err)
+
+				for _, file := range dirFiles.Files {
+					lines, readErr := file.ReadLines(dirFiles.Dir)
+
+					errcore.HandleErr(readErr)
+
+					actualSlice.AppendFmt(
+						"%d : %s",
+						i,
+						file.RelativePath,
+					)
+
+					for lineIndex, line := range lines {
+						actualSlice.AppendFmt(
+							"         %d. %s",
+							lineIndex,
+							line,
+						)
+					}
+				}
+			}
+		}
+
+		finalActLines := actualSlice.Strings()
+
+		// Assert
+		testCase.AssertEqual(
+			t,
+			caseIndex,
+			finalActLines...,
+		)
+	}
+}
