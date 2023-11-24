6d5fed02ece4866096c8ab19605774c971526d9a
Md. Alim Ul Karim <devorg.bd@gmail.com>
2023-11-24 20:34:42 +0600

    Updates

--- tests/integratedtests/GetAssert_SortedArrayNoPrint_test.go
+++ tests/integratedtests/GetAssert_SortedArrayNoPrint_test.go
@@ -1,7 +1,6 @@
 package integratedtests
 
 import (
-	"strings"
 	"testing"
 
 	"gitlab.com/auk-go/core/coredata/corestr"
@@ -9,99 +8,6 @@ import (
 	"gitlab.com/auk-go/core/coretests/args"
 )
 
-func Test_GetAssert_Quick_Verification(t *testing.T) {
-	for caseIndex, testCase := range quickTestCases {
-		// Arrange
-		input := testCase.
-			ArrangeInput.(args.Map)
-		actualSlice := corestr.
-			New.
-			SimpleSlice.
-			Cap(0)
-		asserter := coretests.GetAssert
-		quickFunc := asserter.Quick
-
-		// Act
-		output := quickFunc(
-			input.When(),                        // when
-			input.Actual(),                      // actual
-			input.Expect(),                      // expected
-			input.GetAsIntDefault("counter", 0), // counter
-		)
-
-		actualSlice.Adds(strings.Split(output, "\n")...)
-		finalActLines := actualSlice.Strings()
-
-		// Assert
-		testCase.ShouldBeEqual(
-			t,
-			caseIndex,
-			finalActLines...,
-		)
-	}
-}
-
-func Test_GetAssert_SortedMessage_Verification(t *testing.T) {
-	for caseIndex, testCase := range sortedMessageTestCases {
-		// Arrange
-		input := testCase.
-			ArrangeInput.(args.Map)
-		actualSlice := corestr.
-			New.
-			SimpleSlice.
-			Cap(0)
-		asserter := coretests.GetAssert
-		quickFunc := asserter.SortedMessage
-
-		// Act
-		outputs := quickFunc(
-			input["isPrint"].(bool),
-			input["message"].(string),
-			input["joiner"].(string),
-		)
-
-		actualSlice.Add(outputs)
-		finalActLines := actualSlice.Strings()
-
-		// Assert
-		testCase.ShouldBeEqual(
-			t,
-			caseIndex,
-			finalActLines...,
-		)
-	}
-}
-
-func Test_GetAssert_SortedArray_Verification(t *testing.T) {
-	for caseIndex, testCase := range sortedArrayTestCases {
-		// Arrange
-		input := testCase.
-			ArrangeInput.(args.Map)
-		actualSlice := corestr.
-			New.
-			SimpleSlice.
-			Cap(0)
-		asserter := coretests.GetAssert
-		sortedArray := asserter.SortedArray
-
-		// Act
-		outputs := sortedArray(
-			input["isPrint"].(bool),
-			input["isSort"].(bool),
-			input["message"].(string),
-		)
-
-		actualSlice.Adds(outputs...)
-		finalActLines := actualSlice.Strings()
-
-		// Assert
-		testCase.ShouldBeEqual(
-			t,
-			caseIndex,
-			finalActLines...,
-		)
-	}
-}
 func Test_GetAssert_SortedArrayNoPrint_Verification(t *testing.T) {
 	for caseIndex, testCase := range sortedArrayTestCases {
 		// Arrange
@@ -117,97 +23,6 @@ func Test_GetAssert_SortedArrayNoPrint_Verification(t *testing.T) {
 		// Act
 		outputs := actFunc(
 			input["message"].(string),
-		)
-
-		actualSlice.Adds(outputs...)
-		finalActLines := actualSlice.Strings()
-
-		// Assert
-		testCase.ShouldBeEqual(
-			t,
-			caseIndex,
-			finalActLines...,
-		)
-	}
-}
-
-func Test_GetAssert_StringsToSpaceString_Verification(t *testing.T) {
-	for caseIndex, testCase := range stringsToSpaceStringTestCases {
-		// Arrange
-		input := testCase.
-			ArrangeInput.(args.Map)
-		actualSlice := corestr.
-			New.
-			SimpleSlice.
-			Cap(0)
-		asserter := coretests.GetAssert
-		actFunc := asserter.StringsToSpaceString
-
-		// Act
-		outputs := actFunc(
-			input["spaceCount"].(int),
-			input["lines"].([]string)...,
-		)
-
-		actualSlice.Adds(outputs...)
-		finalActLines := actualSlice.Strings()
-
-		// Assert
-		testCase.ShouldBeEqual(
-			t,
-			caseIndex,
-			finalActLines...,
-		)
-	}
-}
-
-func Test_GetAssert_ToStringsWithTab_Verification(t *testing.T) {
-	for caseIndex, testCase := range toStringsWithTabTestCases {
-		// Arrange
-		input := testCase.
-			ArrangeInput.(args.Map)
-		actualSlice := corestr.
-			New.
-			SimpleSlice.
-			Cap(0)
-		asserter := coretests.GetAssert
-		actFunc := asserter.ToStringsWithTab
-
-		// Act
-		outputs := actFunc(
-			input["spaceCount"].(int),
-			input["any"],
-		)
-
-		actualSlice.Adds(outputs...)
-		finalActLines := actualSlice.Strings()
-
-		// Assert
-		testCase.ShouldBeEqual(
-			t,
-			caseIndex,
-			finalActLines...,
-		)
-	}
-}
-
-func Test_GetAssert_StringsToSpaceStringUsingFunc_Verification(t *testing.T) {
-	for caseIndex, testCase := range stringsToSpaceStringUsingFuncTestCases {
-		// Arrange
-		input := testCase.
-			ArrangeInput.(args.Map)
-		actualSlice := corestr.
-			New.
-			SimpleSlice.
-			Cap(0)
-		asserter := coretests.GetAssert
-		actFunc := asserter.StringsToSpaceStringUsingFunc
-
-		// Act
-		outputs := actFunc(
-			input["spaceCount"].(int),
-			input["converterFunc"].(coretests.ToLineConverterFunc),
-			input["lines"].([]string)...,
 		)
 
 		actualSlice.Adds(outputs...)
