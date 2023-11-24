f4cb7500e3565ab90567b6f2e11dd000b1f281d7
Md. Alim Ul Karim <devorg.bd@gmail.com>
2023-11-24 23:58:18 +0600

    Updates

--- tests/integratedtests/GetAssert_ConvertLinesToDoubleQuoteThenString_test.go
+++ tests/integratedtests/GetAssert_ConvertLinesToDoubleQuoteThenString_test.go
@@ -35,3 +35,32 @@ func Test_ConvertLinesToDoubleQuoteThenString_Verification(t *testing.T) {
 		)
 	}
 }
+
+func Test_AnyToStringDoubleQuoteLine_Verification(t *testing.T) {
+	for caseIndex, testCase := range convertLinesToDoubleQuoteThenStringTestCases {
+		// Arrange
+		actualSlice := corestr.
+			New.
+			SimpleSlice.
+			Cap(20)
+		asserter := coretests.GetAssert
+		params := testCase.Parameters
+		actFunc := asserter.AnyToStringDoubleQuoteLine
+
+		// Act
+		outputLine := actFunc(
+			params.First.(int),
+			params.Second.([]string),
+		)
+
+		actualSlice.Add(outputLine)
+		finalActLines := actualSlice.Strings()
+
+		// Assert
+		testCase.ShouldBeEqual(
+			t,
+			caseIndex,
+			finalActLines...,
+		)
+	}
+}
