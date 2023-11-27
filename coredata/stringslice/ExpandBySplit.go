39a67c7bac4363dbd29862373530fbac08ed8b69
Md. Alim Ul Karim <devorg.bd@gmail.com>
2023-11-27 23:44:12 +0600

    updates

--- coredata/stringslice/ExpandBySplit.go
+++ coredata/stringslice/ExpandBySplit.go
@@ -1,10 +1,10 @@
 package stringslice
 
 // ExpandBySplit Take each slice item, split and add to the new slice array and returns it.
-func ExpandBySplit(slice *[]string, splitter string) *[]string {
-	length := LengthOfPointer(slice)
+func ExpandBySplit(slice []string, splitter string) []string {
+	length := len(slice)
 	if length == 0 {
-		return &[]string{}
+		return []string{}
 	}
 
 	return ExpandBySplits(slice, splitter)
