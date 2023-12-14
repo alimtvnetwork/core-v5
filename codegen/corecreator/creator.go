ab91735ae46dd177ea0567b0c81273e61d95de4b
Md. Alim Ul Karim <devorg.bd@gmail.com>
2023-12-14 12:38:12 +0600

    updates

---
+++ codegen/corecreator/creator.go
@@ -0,0 +1,20 @@
+package corecreator
+
+import (
+	"reflect"
+
+	"gitlab.com/auk-go/core/coretests/args"
+)
+
+type Creator struct {
+	scopeName string
+	scopeMap  args.Map
+}
+
+func (it newCreator) Create(i interface{}) interface{} {
+	return nil
+}
+
+func (it newCreator) CreateByType(rt reflect.Type) interface{} {
+	return nil
+}
--- codegen/corecreator/newCreator.go
+++ codegen/corecreator/newCreator.go
@@ -2,6 +2,9 @@ package corecreator
 
 type newCreator struct{}
 
-func (it newCreator) CreatorMap(scopeName string, additionalMap map[string]interface{}) {
+func (it newCreator) CreatorMap(
+	scopeName string,
+	additionalMaps ...map[string]interface{},
+) Creator {
 
 }
