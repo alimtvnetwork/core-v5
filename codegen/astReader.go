650e084d36fd18baab2e27be7dddf74142a1c4b4
Md. Alim Ul Karim <devorg.bd@gmail.com>
2023-12-15 17:53:41 +0600

    updates

--- codegen/astReader.go
+++ codegen/astReader.go
@@ -6,25 +6,65 @@ import (
 	"go/token"
 
 	"gitlab.com/auk-go/core/errcore"
+	"golang.org/x/tools/go/packages"
 )
 
 type astReader struct {
+	filePath string
+	src      any
+	node     *ast.File
+	parseErr error
+	fileSet  *token.FileSet
 }
 
-func (it astReader) Initialize(filePath string) (*ast.File, error) {
+func (it *astReader) Initialize() (*ast.File, error) {
 	fileSet := token.NewFileSet()
+
 	node, err := parser.ParseFile(
 		fileSet,
-		filePath,
-		nil,
+		it.filePath,
+		it.src,
 		parser.AllErrors,
 	)
 
+	it.fileSet = fileSet
+	it.node = node
+
 	if err != nil {
-		return node, errcore.ParsingFailed.MsgCsvRefError(
+		finalErr := errcore.ParsingFailed.MsgCsvRefError(
 			err.Error(),
-			filePath,
+			it.filePath,
 		)
+
+		it.parseErr = finalErr
+
+		return node, finalErr
 	}
 
+	return node, err
+}
+
+func (it *astReader) InitializeMust() *ast.File {
+	node, err := it.Initialize()
+
+	errcore.HandleErr(err)
+
+	return node
+}
+
+func (it *astReader) Config() *packages.Config {
+	loadConfig := new(packages.Config)
+	loadConfig.Mode = packages.NeedSyntax | packages.NeedTypes | packages.NeedTypesInfo
+	loadConfig.Fset = token.NewFileSet()
+
+	return loadConfig
+}
+
+func (it *astReader) AllPackages() ([]*packages.Package, error) {
+	loadConfig := it.Config()
+	imports, loadErr := packages.Load(
+		loadConfig, "syscall",
+	)
+
+	return imports, loadErr
 }
--- go.mod
+++ go.mod
@@ -5,9 +5,12 @@ go 1.17.8
 require (
 	github.com/smarty/assertions v1.15.1
 	github.com/smartystreets/goconvey v1.8.1
+	golang.org/x/tools v0.7.0
 )
 
 require (
 	github.com/gopherjs/gopherjs v1.17.2 // indirect
 	github.com/jtolds/gls v4.20.0+incompatible // indirect
+	golang.org/x/mod v0.9.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
 )
